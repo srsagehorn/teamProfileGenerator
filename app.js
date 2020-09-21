96e94bfd6aec9f2ddb52aea4dd4ef6e3d7fa45e6
Shannon Sagehorn <srsagehorn@gmail.com>
2020-09-21 14:43:47 -0500

    Prompts working, creates output file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const questions = require("./lib/questions");
+const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,11 +10,52 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const employees = [];
 
+function runAgain() {
+  // ask if they want to add more employees
+  inquirer
+    .prompt({
+      type: "confirm",
+      name: "again",
+      message: "Would you like to add another employee?",
+    })
+    .then(function (addAnother) {
+      // if they do, rerun the ?s
+      if (addAnother.again == true) {
+        init();
+      }
+      //   if not, render the employees
+      if (addAnother.again == false) {
+        // render(employees);
+        fs.writeFile(outputPath, render(employees), (err) =>
+          err ? console.log(err) : console.log(`Generated ${outputPath}`)
+        );
+      }
+    });
+}
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function init() {
+  // ask questions
+  inquirer.prompt(questions.employee).then(function (answers) {
+    //   create repective objects depending on role and push then to the employees array
+    if (answers.role == "Manager") {
+      employees.push(
+        new Manager(answers.name, answers.id, answers.officeNumber)
+      );
+    }
+    if (answers.role == "Intern") {
+      employees.push(new Intern(answers.name, answers.id, answers.school));
+    }
+    if (answers.role == "Engineer") {
+      employees.push(new Engineer(answers.name, answers.id, answers.github));
+    }
+    console.log(employees);
+    runAgain();
+  });
+}
+
+init();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -23,13 +66,3 @@ const render = require("./lib/htmlRenderer");
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
